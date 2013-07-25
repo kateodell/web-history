d3598ecb211bf574f5ecc35d05589c89b786c3f4
Kate Odell <kate.m.odell@gmail.com>
2013-07-25 14:29:34 -0700

    adding alembic files

---
+++ alembic/versions/2e386c75a006_first_revision.py
@@ -0,0 +1,22 @@
+"""first revision
+
+Revision ID: 2e386c75a006
+Revises: None
+Create Date: 2013-07-25 10:57:37.035048
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '2e386c75a006'
+down_revision = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
